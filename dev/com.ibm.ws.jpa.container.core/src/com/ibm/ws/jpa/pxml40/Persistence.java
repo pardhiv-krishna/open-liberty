dd150f02ea9f77f3a46392db9932c651d9b1f07a
pardhiv-krishna <pardhiv.krishna@ibm.com>
2026-08-13 17:54:42 +0530

    JPA 4.0 FAT creation

--- dev/com.ibm.ws.jpa.container.core/src/com/ibm/ws/jpa/pxml40/Persistence.java
+++ dev/com.ibm.ws.jpa.container.core/src/com/ibm/ws/jpa/pxml40/Persistence.java
@@ -236,6 +236,7 @@ public class Persistence {
                                       "excludeUnlistedClasses",
                                       "sharedCacheMode",
                                       "validationMode",
+                                      "defaultToOneFetchType",
                                       "properties"
     })
     public static class PersistenceUnit {
